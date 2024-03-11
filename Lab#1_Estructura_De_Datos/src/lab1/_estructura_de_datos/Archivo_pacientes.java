6abf57cdc2deb6d9be605fb66fafc691b78a69eb
Digaes <digaes05@gmail.com>
2024-03-11 16:49:06 -0500

    Inicio archivos pacientes

---
+++ Lab#1_Estructura_De_Datos/src/lab1/_estructura_de_datos/Archivo_pacientes.java
@@ -0,0 +1,16 @@
+
+package lab1._estructura_de_datos;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+public class Archivo_pacientes {
+    
+ 
+ 
+}
+        
+            
