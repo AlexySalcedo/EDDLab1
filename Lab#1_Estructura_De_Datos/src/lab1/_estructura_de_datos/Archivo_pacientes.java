d42f852aba8f1aed938ec596aa79be9edcc47ffe
Digaes <digaes05@gmail.com>
2024-03-11 21:23:31 -0500

    .

--- Lab#1_Estructura_De_Datos/src/lab1/_estructura_de_datos/Archivo_pacientes.java
+++ Lab#1_Estructura_De_Datos/src/lab1/_estructura_de_datos/Archivo_pacientes.java
@@ -3,14 +3,17 @@ package lab1._estructura_de_datos;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class Archivo_pacientes {
     
- 
- 
+  private void CrearArchivoPacientes(){
+      File archivoP;
+      archivoP = new File ("ArchivoPacientes.txt");
+  }
 }
         
             
--- Lab#1_Estructura_De_Datos/src/lab1/_estructura_de_datos/Pacientes.java
+++ Lab#1_Estructura_De_Datos/src/lab1/_estructura_de_datos/Pacientes.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package lab1._estructura_de_datos;
 
-/**
- *
- * @author dipae
- */
+
 public class Pacientes {
+    double cedula;
+    String nombre,apellido,especialidadMedica,medicoAsignado;
+    
+    public paciente(){
+        cedula = 0;
+        nombre = "";
+        apellido= "";
+        especialidadMedica="";
+        medicoAsignado="";
+    }
+
+    public double getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(double cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEspecialidadMedica() {
+        return especialidadMedica;
+    }
+
+    public void setEspecialidadMedica(String especialidadMedica) {
+        this.especialidadMedica = especialidadMedica;
+    }
+
+    public String getMedicoAsignado() {
+        return medicoAsignado;
+    }
+
+    public void setMedicoAsignado(String medicoAsignado) {
+        this.medicoAsignado = medicoAsignado;
+    }
+    
     
 }
