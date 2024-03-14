29310a403436ec401bbcae80fb2786ab4b5cb09d
AlexySalcedo <alexysalcedo@icloud.com>
2024-03-13 22:36:51 -0500

    bb

--- EstructuraDeDatosLab#1/src/estructuradedatoslab/pkg1/paginasDelView/PersonasYmedicosController.java
+++ EstructuraDeDatosLab#1/src/estructuradedatoslab/pkg1/paginasDelView/PersonasYmedicosController.java
@@ -26,6 +26,7 @@ import javafx.stage.Stage;
  * @author alexysalcedo
  */
 public class PersonasYmedicosController implements Initializable {
+    PacientespageController pc= new PacientespageController();
 
     @FXML
     private AnchorPane fondo;
@@ -49,6 +50,7 @@ public class PersonasYmedicosController implements Initializable {
         stage.setScene(scene);
         stage.show();
         Main.cerrar(event);
+        pc.CargarTabla();
     }
     
 
