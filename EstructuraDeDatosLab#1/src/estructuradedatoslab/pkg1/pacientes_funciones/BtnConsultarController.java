c22d15a0199188e362fea20a96d90b97723066f5
AlexySalcedo <alexysalcedo@icloud.com>
2024-03-14 02:59:54 -0500

    lll

--- EstructuraDeDatosLab#1/src/estructuradedatoslab/pkg1/pacientes_funciones/BtnConsultarController.java
+++ EstructuraDeDatosLab#1/src/estructuradedatoslab/pkg1/pacientes_funciones/BtnConsultarController.java
@@ -4,9 +4,19 @@
  */
 package estructuradedatoslab.pkg1.pacientes_funciones;
 
+import estructuradedatoslab.pkg1.Backend.Archivo_pacientes;
+import estructuradedatoslab.pkg1.Backend.Pacientes;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -15,12 +25,58 @@ import javafx.fxml.Initializable;
  */
 public class BtnConsultarController implements Initializable {
 
+    @FXML
+    private TextField setId;
+    @FXML
+    private TableView<Pacientes> tblConsultar;
+    @FXML
+    private TableColumn colIdentificacion;
+    @FXML
+    private TableColumn colNombre;
+    @FXML
+    private TableColumn colApellido;
+    @FXML
+    private TableColumn colEspecialidad;
+    @FXML
+    private TableColumn colMedicoAsignado;
+    
+    private ObservableList <Pacientes> paciente; 
+
     /**
      * Initializes the controller class.
+     * 
      */
+    
+    Archivo_pacientes pacien = new Archivo_pacientes();
+    @FXML
+    private TableColumn<?, ?> colFechaCita;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        paciente = FXCollections.observableList(pacien.Leer());
+        this.colIdentificacion.setCellValueFactory(new PropertyValueFactory("cedula"));
+        this.colNombre.setCellValueFactory(new PropertyValueFactory("nombre"));
+        this.colFechaCita.setCellValueFactory(new PropertyValueFactory("fecha"));
+        this.colMedicoAsignado.setCellValueFactory(new PropertyValueFactory("nombre"));
+        this.colEspecialidad.setCellValueFactory(new PropertyValueFactory("especialidadMedica"));
+        this.colApellido.setCellValueFactory(new PropertyValueFactory("apellido"));
     }    
+
+    @FXML
+    private void buscar(ActionEvent event) {
+         String id= setId.getText();
+         long idc= Long.parseLong(id);
+         paciente = FXCollections.observableList(pacien.Consultar(idc));
+         this.tblConsultar.setItems(paciente);
+         
+         
+    }
+
+    @FXML
+    private void eliminar(ActionEvent event) {
+    }
+
+    @FXML
+    private void eliminartodo(ActionEvent event) {
+    }
     
 }
